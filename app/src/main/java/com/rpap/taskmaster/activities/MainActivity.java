306bcc095437f7c4b0662b668608ae28710b65b0
Ryan Papsin <113944887+rpap75@users.noreply.github.com>
2023-02-15 16:25:55 -0700

    Main page TextView Task taking to Task Details

--- app/src/main/java/com/rpap/taskmaster/activities/MainActivity.java
+++ app/src/main/java/com/rpap/taskmaster/activities/MainActivity.java
@@ -18,12 +18,38 @@ import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final String TAG = "MainActivity";
+    public static final String TASKS_EXTRA_TAG = "userTask";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mainActivityTaskOneTextViewBtn
+
+        TextView taskDetailButtonOne = (TextView) findViewById(R.id.mainActivityTaskOneTextViewBtn);
+        taskDetailButtonOne.setOnClickListener(v -> {
+            String taskDetail = ((TextView) findViewById(R.id.mainActivityTaskOneTextViewBtn)).getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASKS_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
+
+        TextView taskDetailButtonTwo = (TextView) findViewById(R.id.mainActivityTaskTwoTextViewBtn);
+        taskDetailButtonTwo.setOnClickListener(v -> {
+            String taskDetail = ((TextView) findViewById(R.id.mainActivityTaskTwoTextViewBtn)).getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASKS_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
+
+        TextView taskDetailButtonThree = (TextView) findViewById(R.id.mainActivityTaskThreeTextViewBtn);
+        taskDetailButtonThree.setOnClickListener(v -> {
+            String taskDetail = ((TextView) findViewById(R.id.mainActivityTaskThreeTextViewBtn)).getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASKS_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
 
         ImageView settingsButton = (ImageView) findViewById(R.id.mainActivitySettingsImageView);
         settingsButton.setOnClickListener(v -> {
--- app/src/main/java/com/rpap/taskmaster/activities/TaskDetailActivity.java
+++ app/src/main/java/com/rpap/taskmaster/activities/TaskDetailActivity.java
@@ -2,7 +2,9 @@ package com.rpap.taskmaster.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.rpap.taskmaster.R;
 
@@ -12,5 +14,17 @@ public class TaskDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+
+        Intent callingIntent = getIntent();
+        String userTaskString = null;
+        if (callingIntent != null) {
+            userTaskString = callingIntent.getStringExtra(MainActivity.TASKS_EXTRA_TAG);
+            TextView userTasksTextView = (TextView) findViewById(R.id.taskDetailActivityTitle);
+            if (userTaskString != null) {
+                userTasksTextView.setText(userTaskString);
+            } else {
+                userTasksTextView.setText("No Input");
+            }
+        }
     }
 }
