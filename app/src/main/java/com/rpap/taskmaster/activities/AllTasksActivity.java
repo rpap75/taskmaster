23b39215a74cbc2f462efdaa19fd9dad6ac142a6
Ryan Papsin <113944887+rpap75@users.noreply.github.com>
2023-02-20 17:59:08 -0700

    lab 31 turned in

--- app/src/main/java/com/rpap/taskmaster/activities/AllTasksActivity.java
+++ app/src/main/java/com/rpap/taskmaster/activities/AllTasksActivity.java
@@ -17,40 +17,40 @@ import com.rpap.taskmaster.model.task;
 import java.util.List;
 
 public class AllTasksActivity extends AppCompatActivity {
-//    taskMasterDatabase taskMasterDatabase;
-//    public static final String DATABASE_NAME = "task_master";
-//
-//
-//    List<task> taskList;
-//    taskRecyclerViewAdapter adapter;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_all_tasks);
-//
-//        taskMasterDatabase = Room.databaseBuilder(
-//                        getApplicationContext(),
-//                        taskMasterDatabase.class,
-//                        DATABASE_NAME)
-//                .fallbackToDestructiveMigration() //DO NOT USE IN PRODUCTION
-//                .allowMainThreadQueries()
-//                .build();
-//
-//        taskList = taskMasterDatabase.taskDao().findAll();
-//        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-//
-//        setUpRecyclerView();
-//
-//    }
-//
-//    public void setUpRecyclerView() {
-//
-//        RecyclerView tasksRecyclerView = findViewById(R.id.allTaskActivityRecyclerViewTasks);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-//        tasksRecyclerView.setLayoutManager(layoutManager);
-//
-//        adapter = new taskRecyclerViewAdapter(taskList, this);
-//        tasksRecyclerView.setAdapter(adapter);
-//    }
+    taskMasterDatabase taskMasterDatabase;
+    public static final String DATABASE_NAME = "task_master";
+
+
+    List<task> taskList;
+    taskRecyclerViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks);
+
+        taskMasterDatabase = Room.databaseBuilder(
+                        getApplicationContext(),
+                        taskMasterDatabase.class,
+                        DATABASE_NAME)
+                .fallbackToDestructiveMigration() //DO NOT USE IN PRODUCTION
+                .allowMainThreadQueries()
+                .build();
+
+        taskList = taskMasterDatabase.taskDao().findAll();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setUpRecyclerView();
+
+    }
+
+    public void setUpRecyclerView() {
+
+        RecyclerView tasksRecyclerView = findViewById(R.id.allTaskActivityRecyclerViewTasks);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        tasksRecyclerView.setLayoutManager(layoutManager);
+
+        adapter = new taskRecyclerViewAdapter(taskList, this);
+        tasksRecyclerView.setAdapter(adapter);
+    }
 }
